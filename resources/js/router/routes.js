56fd53ee40a13bc0dcbcfbe2317755ae332c612e
julesinger <jules.singer6@etu.univ-lorraine.fr>
2021-10-21 14:24:05 +0200

    ci: configure laravel-mix instead of pure webpack

--- resources/js/router/routes.js
+++ resources/js/router/routes.js
@@ -1,26 +1,30 @@
+import Home from '../components/Home.vue'
+import Article from '../components/articles/Article.vue'
+import Quotes from '../components/quotes/Quotes.vue'
+import About from '../components/About.vue'
 export const routes = [
   {
     path: '/',
     name: 'Home',
-    component: () => import('../Home.vue') ,
+    component: Home ,
     meta: { title: 'Accueil' }
   },
   {
     path: '/article',
     name: 'Article',
-    component: () => import('../articles/Article.vue'),
+    component: Article,
     meta: { title: 'Article' }
   },
   {
     path: '/quotes',
     name: 'Quotes',
-    component: () => import('../quotes/Quotes.vue'),
+    component: Quotes,
     meta: { title: 'Citations' }
   },
    {
     path: '/about',
     name: 'About',
-    component: () => import('../About.vue'),
+    component: About,
     meta: { title: 'A Propos' }
   }
 ]
--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -4,7 +4,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>L'OpenBlog - {{ $title }}</title> 
     <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Abhaya+Libre&family=Nunito&display=swap" rel="stylesheet"> 
     @section('stylesheets')
         <link rel="stylesheet" href="/css/app.css">
     @show
