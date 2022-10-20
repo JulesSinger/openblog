4871189d9379990c25124f5cbc1eb327036094ab
SINGER Jules <jules.singer@telecomnancy.eu>
2022-10-20 20:35:11 +0200

    feat: handle the case of a disconnected user  for comments

--- resources/js/components/comments/CommentCreate.vue
+++ resources/js/components/comments/CommentCreate.vue
@@ -1,5 +1,5 @@
 <template>
-    <form class="comment-post" @submit.prevent="call_post_comment">
+    <form v-if="isLogged" class="comment-post" @submit.prevent="call_post_comment">
         <label for="content">Ajouter un commentaire :</label>
         <input placeholder="Objet du commentaire" type="text" name="subject" id="subject" v-model="form.subject">
         <textarea placeholder="Contenu de votre commentaire ici" name="content" id="content" v-model="form.content"></textarea>
@@ -7,21 +7,31 @@
             <button type="submit" class="btn btn-markup">POSTER LE COMMENTAIRE</button>
         </div>
     </form>
+    <div v-else class="mt-2 ml-3 flex">
+        <router-link :to="{name: 'Login', query: { redirect: fullPath}}" class="btn btn-markup w-7">Connecte toi !</router-link>
+    </div>
 </template>
 
 <script setup>
 
-import { reactive } from 'vue'
+import { reactive, computed } from 'vue'
 import { useComments } from '../../api/comments.js'
 import { useRoute } from 'vue-router'
+import { useStore } from 'vuex'
 
-// route
+// route & store
 const route = useRoute()
+const store = useStore()
 
 /**
  * post_id get by url params
  */
 const id = route.params.id
+
+/**
+ * full path is used to redirect on the current page after login 
+ */
+const fullPath = route.fullPath
 
 /**
  * this form is linked to the post comment form in html
@@ -31,6 +41,11 @@ const form = reactive({
     subject: '',
     post_id : id,
 })
+
+/**
+ * true if the user is logged, false in the other case
+ */
+ const isLogged = computed(() => store.state.auth.authenticated)
 
 /**
  * load comment api
