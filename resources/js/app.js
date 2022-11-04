6f725457bb30ad14197263b5ce5d34b6f3e505f3
SINGER Jules <jules.singer@telecomnancy.eu>
2022-11-04 11:20:45 +0100

    chore: add a vue markdown library

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,10 +4,12 @@ import Router from './router/router.js'
 import store from './store'
 import App from './App.vue'
 import Toaster from '@meforma/vue-toaster';
+import Markdown from 'vue3-markdown-it'
 import { createApp } from 'vue/dist/vue.esm-bundler';
 
 const app = createApp(App)
 app.use(Router)
+app.use(Markdown)
 app.use(store)
 app.use(Toaster).provide('toast', app.config.globalProperties.$toast);
 app.mount('#app')
